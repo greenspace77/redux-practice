4175bf9b00f630494c584cb4218ef26d8eaeaae5
greenspace77 <greenspace@sk.com>
2025-02-21 11:42:19 +0900

    redux 실습 프로젝트

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    width: 100%
+`;
+
+function App() {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+}
+
+export default App;
